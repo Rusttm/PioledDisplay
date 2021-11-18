4027c8855473d0d8c9aa5a44a5d6106ce4c04bf0
Rustam <rust_mazh@mail.ru>
2021-11-19 00:34:44 +0300

    Update pidisplay.py

--- pidisplay.py
+++ pidisplay.py
@@ -1,93 +1,47 @@
 # SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
 # SPDX-License-Identifier: MIT
 
-"""
-Be sure to check the learn guides for more usage information.
-
-This example is for use on (Linux) computers that are using CPython with
-Adafruit Blinka to support CircuitPython libraries. CircuitPython does
-not support PIL/pillow (python imaging library)!
-
-Author(s): Melissa LeBlanc-Williams for Adafruit Industries
-"""
-
 import digitalio
 import board
-from PIL import Image, ImageDraw
-# from adafruit_rgb_display import ili9341
-from adafruit_rgb_display import st7789  # pylint: disable=unused-import
-# from adafruit_rgb_display import hx8357  # pylint: disable=unused-import
-# from adafruit_rgb_display import st7735  # pylint: disable=unused-import
-# from adafruit_rgb_display import ssd1351  # pylint: disable=unused-import
-# from adafruit_rgb_display import ssd1331  # pylint: disable=unused-import
 
-# Configuration for CS and DC pins (these are PiTFT defaults):
+from adafruit_rgb_display.rgb import color565
+from adafruit_rgb_display import st7789
+
+# Configuration for CS and DC pins for Raspberry Pi
 cs_pin = digitalio.DigitalInOut(board.CE0)
 dc_pin = digitalio.DigitalInOut(board.D25)
-reset_pin = digitalio.DigitalInOut(board.D24)
-
-# Config for display baudrate (default max is 24mhz):
-BAUDRATE = 24000000
-
-# Setup SPI bus using hardware SPI:
-spi = board.SPI()
-
-# pylint: disable=line-too-long
-# Create the display:
-# disp = st7789.ST7789(spi, rotation=90,                            # 2.0" ST7789
-disp = st7789.ST7789(spi, height=240, y_offset=240, rotation=180,  # 1.3", 1.54" ST7789
-# disp = st7789.ST7789(spi, rotation=90, width=135, height=240, x_offset=53, y_offset=40, # 1.14" ST7789
-# disp = hx8357.HX8357(spi, rotation=180,                           # 3.5" HX8357
-# disp = st7735.ST7735R(spi, rotation=90,                           # 1.8" ST7735R
-# disp = st7735.ST7735R(spi, rotation=270, height=128, x_offset=2, y_offset=3,   # 1.44" ST7735R
-# disp = st7735.ST7735R(spi, rotation=90, bgr=True,                 # 0.96" MiniTFT ST7735R
-# disp = ssd1351.SSD1351(spi, rotation=180,                         # 1.5" SSD1351
-# disp = ssd1351.SSD1351(spi, height=96, y_offset=32, rotation=180, # 1.27" SSD1351
-# disp = ssd1331.SSD1331(spi, rotation=180,                         # 0.96" SSD1331
-# disp = ili9341.ILI9341(
-#     spi,
-#     rotation=90,  # 2.2", 2.4", 2.8", 3.2" ILI9341
+reset_pin = None
+BAUDRATE = 64000000  # The pi can be very fast!
+# Create the ST7789 display:
+display = st7789.ST7789(
+    board.SPI(),
     cs=cs_pin,
     dc=dc_pin,
     rst=reset_pin,
     baudrate=BAUDRATE,
+    width=135,
+    height=240,
+    x_offset=53,
+    y_offset=40,
 )
-# pylint: enable=line-too-long
 
-# Create blank image for drawing.
-# Make sure to create image with mode 'RGB' for full color.
-if disp.rotation % 180 == 90:
-    height = disp.width  # we swap height/width to rotate it to landscape!
-    width = disp.height
-else:
-    width = disp.width  # we swap height/width to rotate it to landscape!
-    height = disp.height
-image = Image.new("RGB", (width, height))
+backlight = digitalio.DigitalInOut(board.D22)
+backlight.switch_to_output()
+backlight.value = True
+buttonA = digitalio.DigitalInOut(board.D23)
+buttonB = digitalio.DigitalInOut(board.D24)
+buttonA.switch_to_input()
+buttonB.switch_to_input()
 
-# Get drawing object to draw on image.
-draw = ImageDraw.Draw(image)
-
-# Draw a black filled box to clear the image.
-draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
-disp.image(image)
-
-image = Image.open("blinka.jpg")
-
-# Scale the image to the smaller screen dimension
-image_ratio = image.width / image.height
-screen_ratio = width / height
-if screen_ratio < image_ratio:
-    scaled_width = image.width * height // image.height
-    scaled_height = height
-else:
-    scaled_width = width
-    scaled_height = image.height * width // image.width
-image = image.resize((scaled_width, scaled_height), Image.BICUBIC)
-
-# Crop and center the image
-x = scaled_width // 2 - width // 2
-y = scaled_height // 2 - height // 2
-image = image.crop((x, y, x + width, y + height))
-
-# Display image.
-disp.image(image)
+# Main loop:
+while True:
+    if buttonA.value and buttonB.value:
+        backlight.value = False  # turn off backlight
+    else:
+        backlight.value = True  # turn on backlight
+    if buttonB.value and not buttonA.value:  # just button A pressed
+        display.fill(color565(255, 0, 0))  # red
+    if buttonA.value and not buttonB.value:  # just button B pressed
+        display.fill(color565(0, 0, 255))  # blue
+    if not buttonA.value and not buttonB.value:  # none pressed
+        display.fill(color565(0, 255, 0))  # green
