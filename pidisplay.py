eb5dbb294e7c6e85d2d5b6ed5535f2a1b83f7e6e
Rustam <rust_mazh@mail.ru>
2021-11-18 23:11:47 +0300

    Update pidisplay.py

--- pidisplay.py
+++ pidisplay.py
@@ -1,4 +1,9 @@
 from digitalio import DigitalInOut
+import time
+import board
+import digitalio
+import pwmio
+import adafruit_character_lcd.character_lcd as characterlcd
 
 # Turn on the Backlight
 backlight = DigitalInOut(board.D26)
