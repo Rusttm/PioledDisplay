744b8dfdf335075c8e49a077f74708ae940bcc1d
pi <rustammazhtov@gmail.com>
2024-07-18 08:17:20 -0900

    cv2 on camera works

--- rpi4_cam.py
+++ rpi4_cam.py
@@ -2,5 +2,12 @@
 import cv2
 # import picamera2
 cap = cv2.VideoCapture(0)
-result, frame = cap.read()
-print(result, frame)
+while True:
+
+    result, frame = cap.read()
+    # print(result, frame)
+    cv2.imshow("PI Camera", frame)
+    if cv2.waitKey(1) == ord("q"):
+        break
+cap.release()
+cv2.destroyAllWindows()
